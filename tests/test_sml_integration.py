de057b912a4317f61af51bc924b971cd43d9f8a2
wegfawefgawefg <668es218pur@gmail.com>
2026-07-15 01:02:06 +0000

    Register SML pit death scenario

--- tests/test_sml_integration.py
+++ tests/test_sml_integration.py
@@ -106,6 +106,7 @@ class SmlResearchIntegrationTest(unittest.TestCase):
                 'sml.nokobon-stomp-bomb',
                 'sml.opening-obstacles',
                 'sml.pipe-room',
+                'sml.pit-death',
                 'sml.plain-block-small',
                 'sml.second-pipe-room',
                 'sml.star',
